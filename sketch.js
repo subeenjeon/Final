538da8976bd74b23ffd017a14e441a7b3d649078
subeenjeon <powersubeen@naver.com>
2014-12-04 14:56:33 +0900

    f

--- sketch.js
+++ sketch.js
@@ -9,19 +9,21 @@ var waveform = [];
 function preload(){
 	soundFile = loadSound('./ODESZA - Bloom.mp3');
 }
+
 function setup() {
   createCanvas(fftBands, 500);
   fill(255, 40, 255);
   stroke(240);
   strokeWeight(4);
+  soundFile.loop(); 
+  // create a new Amplitude analyzer
+  analyzer = new p5.Amlitude();
+  // Patch the input to an volume analyzer
+  analyzer.sotInput(song);
 
   fft = new p5.FFT(0, fftBands);
 
-
-console.log("dddddD");
-  
-  
-  soundFile.play();
+ 
 }
 
 function draw() {
@@ -33,9 +35,14 @@ function draw() {
   fill(255,map(waveform[0], 0, 256, 0,255));
   	rect(0,0,width,height);
 
+ // Get the overall volume (between 0 and 1.0)
+  var vol = analyzer.getLevel();
+  fill(127);
+  stroke(0);
 
-  // map the oscillator frequency to mouse position
- 
+  // Draw an ellipse with size based on volume
+  ellipse(width/2, height/2, 10+vol*200, 10+vol*200);
+
 }
 
 // TOGGLE INPUT
