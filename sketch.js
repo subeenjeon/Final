ce7b04df8a617f863cc71f763e642d0b119e09d7
subeenjeon <powersubeen@naver.com>
2014-12-04 13:48:06 +0900

    aaa

--- sketch.js
+++ sketch.js
@@ -1,45 +1,100 @@
-var a = new Array(800);
-var sound;
-function preload(){
-	sound = loadSound('ODESZA - Bloom.mp3');
+var soundFile, mic, osc;
+
+var fft;
+var fftBands = 1024;
+
+// Array of amplitude values (0-255) over time.
+var waveform = [];
+
+function setup() {
+  createCanvas(fftBands, 500);
+  fill(255, 40, 255);
+  stroke(240);
+  strokeWeight(4);
+
+  fft = new p5.FFT(0, fftBands);
+
+  // set up other inputs we may use by toggling input
+  mic = new p5.AudioIn();
+  osc = new p5.Oscillator();
+  osc.amp(0.5);
+  osc.freq(10);
+
+
+  mic.start();
+  fft.setInput(mic);
+
+  // load the soundfile in setup, but we won't play it until user hits 'T'
+  soundFile = loadSound('ODESZA - Bloom.mp3');
 }
 
-function setup(){
+function draw() {
+  background(30, 30, 30, 220);
 
-	createCanvas(800,600);
-	sound.play();
+  /** 
+   * Analyze the sound as a waveform (amplitude over time)
+   */
+  waveform = fft.waveform();
+
+  // draw snapshot of the waveform
+  beginShape();
+  for (var i = 0; i<waveform.length; i++){
+    var x = map(i, 0, waveform.length, 0, width);
+    var y = map(waveform[i], 0, 256, -height/2, height/2);
+    vertex(x, y + height/2);
+  }
+  endShape();
+
+  // map the oscillator frequency to mouse position
+  var freq = map(mouseX, 0, windowWidth, 1, 440);
+  osc.freq(freq);
 }
 
-
-function draw(){
-	background(255);
-	if(sound.isPlaying()){
-		fill(0,255,0);
-	}else{
-		fill(255,0,0);
-	}
-	noStroke();
-	for(var i = 0; i < 800 ; i++){
-		rect(i,0,1,sound.getPeaks(a)[i]);
-	}
-
-
+// TOGGLE INPUT
+function keyPressed() {
+  if (key == 'T') {
+    toggleInput();
+  }
 }
 
+// start with mic as input
+var inputMode = 1;
 
-// var s = function( sketch ) {
-//   var drums, follow
-  
-//   sketch.setup = function() {
-//     sketch.createCanvas( sketch.windowWidth, sketch.windowHeight )
-
-//     drums = sketch.Gibber.Percussion.EDrums('x*x*x*o-')
-//     follow = sketch.Gibber.Analysis.Follow( drums )
-//   };
-
-//   sketch.draw = function() {
-//     sketch.background( follow.getValue() * 255 )
-//   }
-// }
-
-// var myp5 = new p5( s )
+function toggleInput() {
+  inputMode += 1;
+  inputMode = inputMode % 6;
+  switch (inputMode) {
+    case 0: // soundFile mode
+      soundFile.play();
+      osc.stop();
+      fft.setInput(soundFile);
+      currentSource = 'soundFile';
+      break;
+    case 1: // mic mode
+      mic.start();
+      soundFile.pause();
+      fft.setInput(mic);
+      currentSource = 'mic';
+      break;
+    case 2: // sine mode
+      osc.setType('sine');
+      osc.start();
+      soundFile.pause();
+      mic.stop();
+      fft.setInput(osc);
+      currentSource = 'sine';
+      break;
+    case 3: // square mode
+      osc.setType('triangle');
+      currentSource = 'triangle';
+      break;
+    case 4: // square mode
+      osc.setType('square');
+      currentSource = 'square';
+      break;
+    case 5: // square mode
+      osc.setType('sawtooth');
+      currentSource = 'sawtooth';
+      break;
+  }
+}
