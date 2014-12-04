649e185a9cc6ed8ce3efaf4cdc9b685eab3dde01
subeenjeon <powersubeen@naver.com>
2014-12-04 15:46:25 +0900

    dddd

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ function preload(){
 function setup() {
   createCanvas(2000, 1000);
   background(0);
-  pattern = loadImage("home-pattern.jpg");
+  //pattern = loadImage("home-pattern.jpg");
   for(var i = 0; i<50 ;i++){
     checkDraw[i] = true;
   }
@@ -24,7 +24,7 @@ function draw() {
   image(pattern,0,0);
 
   for(var i = 0 ; i <10 ; i++){
-    for(var j = 0 ; j <5 ; i++){
+    for(var j = 0 ; j <5 ; j++){
       if(checkDraw[i+j*5]){
         fill(10*i,20*j,0);
         rect(i*200, j*200,200,200);
