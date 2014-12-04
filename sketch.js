8588eba43bf84ebd9881244a2cb2a22f1df14ac6
subeenjeon <powersubeen@naver.com>
2014-12-04 15:56:08 +0900

    aaaa

--- sketch.js
+++ sketch.js
@@ -19,13 +19,8 @@ function draw() {
   var a = map(amp.getLevel(),0,1,0,50);
   for(var i = 0 ; i <10 ; i++){
     for(var j = 0 ; j <5 ; j++){
-
-        fill(10*i,20*j,0);
-        rect(i*200, j*200,200,200);
         image(pt,i*200-a/2,j*200-a/2,200+a.200+a);
-
-
-    }
+     }
   }
 
 }
