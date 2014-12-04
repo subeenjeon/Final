6b6ce4ea5199b4bd3de37e39c84d572d850bcc0c
subeenjeon <powersubeen@naver.com>
2014-12-04 17:08:42 +0900

    afsd

--- sketch.js
+++ sketch.js
@@ -3,7 +3,6 @@ var checkDraw = [];
 var sound;
 
 var fft;
-var fftBands = 2000;
 
 var count;
 
@@ -13,11 +12,8 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(fftBands, 1000);
-  fill(255, 40, 255);
-  stroke(240);
-  strokeWeight(4);
-
+  createCanvas(2000, 1000);
+  background(0);
   pt = loadImage("./home-pattern.jpg");
   count = 0;
 
@@ -33,7 +29,6 @@ function draw() {
 
   fill(255,map(waveform[0], 0, 256, 0,255));
     rect(0,0,width,height);
-
   for(var i = 0 ; i <10 ; i++){
     for(var j = 0 ; j <6 ; j++){
         image(pt,i*200,j*200-count,200,200);
