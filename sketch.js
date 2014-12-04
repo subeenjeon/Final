f8eb96d2ba47eaed86dd0de6dd8608186cdf90e2
subeenjeon <powersubeen@naver.com>
2014-12-04 14:21:02 +0900

    test

--- sketch.js
+++ sketch.js
@@ -16,33 +16,18 @@ function setup() {
 
   fft = new p5.FFT(0, fftBands);
 
-  // set up other inputs we may use by toggling input
-
 
 console.log("dddddD");
   
-  
-  // load the soundfile in setup, but we won't play it until user hits 'T'
   
   soundFile.play();
 }
 
 function draw() {
-  background(30, 30, 30, 220);
+  background(0);
 
-  /** 
-   * Analyze the sound as a waveform (amplitude over time)
-   */
   waveform = fft.waveform();
 
-  // draw snapshot of the waveform
-  // beginShape();
-  // for (var i = 0; i<waveform.length; i++){
-  //   var x = map(i, 0, waveform.length, 0, width);
-  //   var y = map(waveform[i], 0, 256, -height/2, height/2);
-  //   vertex(x, y + height/2);
-  // }
-  // endShape();
 
   fill(255,map(waveform[0], 0, 256, 0,255));
   	rect(0,0,width,height);
@@ -71,32 +56,6 @@ function toggleInput() {
       osc.stop();
       fft.setInput(soundFile);
       currentSource = 'soundFile';
-      break;
-    case 1: // mic mode
-      mic.start();
-      soundFile.pause();
-      fft.setInput(mic);
-      currentSource = 'mic';
-      break;
-    case 2: // sine mode
-      osc.setType('sine');
-      osc.start();
-      soundFile.pause();
-      mic.stop();
-      fft.setInput(osc);
-      currentSource = 'sine';
-      break;
-    case 3: // square mode
-      osc.setType('triangle');
-      currentSource = 'triangle';
-      break;
-    case 4: // square mode
-      osc.setType('square');
-      currentSource = 'square';
-      break;
-    case 5: // square mode
-      osc.setType('sawtooth');
-      currentSource = 'sawtooth';
       break;
   }
 }
