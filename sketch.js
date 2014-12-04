21067033884fb4aa8ddd89cd34201f070f886681
subeenjeon <powersubeen@naver.com>
2014-12-04 17:09:23 +0900

    adf

--- sketch.js
+++ sketch.js
@@ -1,12 +1,10 @@
 var pt;
 var checkDraw = [];
 var sound;
+var a; 
 
-var fft;
 
 var count;
-
-var waveform = [];
 function preload(){
   sound = loadSound("./ODESZA - Bloom.mp3")
 }
@@ -15,20 +13,14 @@ function setup() {
   createCanvas(2000, 1000);
   background(0);
   pt = loadImage("./home-pattern.jpg");
+  amp = new p5.Amplitude();
+  sound.loop();
   count = 0;
 
-  fft = new p5.FFT(0, fftBands);
- 
-  sound.loop();
 }
 
 function draw() {
   background(0);
-
-  waveform = fft.waveform();
-
-  fill(255,map(waveform[0], 0, 256, 0,255));
-    rect(0,0,width,height);
   for(var i = 0 ; i <10 ; i++){
     for(var j = 0 ; j <6 ; j++){
         image(pt,i*200,j*200-count,200,200);
