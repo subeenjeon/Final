e641cc2701e3ac5c30e6b08e34f64bb024f486fc
subeenjeon <powersubeen@naver.com>
2014-12-08 15:29:53 +0900

    f

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function draw() {
 function word(){
     fill(Math.random()*255,Math.random()*255,Math.random()*255,100);
     stroke(255);
-    textSize(20);
+    textSize(120);
     textFont("Helvetica");
-    text("Subeen Jeon", 130, 250);
+    text("Subeen Jeon", 500, 1000);
 }
